e8b7d103f29d3395c276a236b9b296ac1e7040c0
horuscod <jessicabueno.contato2022@gmail.com>
2023-11-28 03:56:54 -0300

    adjust context

---
+++ src/Context/AuthUserContext.js
@@ -0,0 +1,17 @@
+// AuthorizedUserContext.js
+import React, { createContext, useContext, useState } from 'react';
+
+const AuthorizedUserContext = createContext();
+export function useAuthorizedUser() {
+  return useContext(AuthorizedUserContext);
+}
+
+export function AuthorizedUserProvider({ children }) {
+  const [authorizedUser, setAuthorizedUser] = useState(false);
+
+  return (
+    <AuthorizedUserContext.Provider value={{ authorizedUser, setAuthorizedUser }}>
+      {children}
+    </AuthorizedUserContext.Provider>
+  );
+}
