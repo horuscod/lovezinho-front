e4dd5f1f7ef0db92c0f271d63462e88740aae594
horuscod <jessicabueno.contato2022@gmail.com>
2023-11-28 15:19:36 -0300

    strucut profile

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Login from "./Views/Login";
 import Home from "./Views/Home";
+import Profile from "./Views/Profile";
 import { AuthorizedUserProvider } from "./Context/AuthUserContext";
 
 function App() {
   return (
     <AuthorizedUserProvider>
-        <MainContent />
+      <MainContent />
     </AuthorizedUserProvider>
   );
 }
 
 function MainContent() {
-  return (
+  return (  
     <Router>
       <Routes>
         <Route exact path="/login" element={<Login />} />
       </Routes>
       <Routes>
         <Route exact path="/find-matches" element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
