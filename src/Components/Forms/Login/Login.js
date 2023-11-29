5c85c5972b6b5a9b96fecd36ba21bf24690419dc
horuscod <jessicabueno.contato2022@gmail.com>
2023-11-29 16:22:55 -0300

    adjust component login, commit in import UserService. We will use firebase AUTH

--- src/Components/Forms/Login/Login.js
+++ src/Components/Forms/Login/Login.js
@@ -1,62 +1,61 @@
-import React, {useState} from 'react'
-import { Container, Form, SubContainerSign } from './Styles.js'
-import Input from '../../Input/index.js'
-import Button from '../../Button/index.js'
-import { validateEmail, validatePassword } from '../../Val/validate.js'
-import UserService from '../../Services/UserService.js'
-import { NavLink, useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { Container, Form, SubContainerSign } from "./Styles.js";
+import Input from "../../Input/index.js";
+import Button from "../../Button/index.js";
+import { validateEmail, validatePassword } from "../../Val/validate.js";
+//import UserService from '../../Services/UserService.js'
+import { NavLink, useNavigate } from "react-router-dom";
 
-const userService = new UserService()
+//const userService = new UserService();
 
 const Login = () => {
-  const [loading, setLoading] = useState()
-  const [form, setForm] = useState([])
-  const navigate = useNavigate()
+  const [loading, setLoading] = useState();
+  const [form, setForm] = useState([]);
+  const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      setLoading(true)
-      const response = await userService.login(form)
-      console.log('response do Login', response)
+      setLoading(true);
+      const response = false;
+      console.log("response do Login", response);
       if (response === true) {
-        alert('usuário Logado com Sucesso')
-        navigate('/home')
+        alert("usuário Logado com Sucesso");
+        navigate("/home");
       }
-      setLoading(false)
+      setLoading(false);
+    } catch (err) {
+      alert("Algo deu errado com o Login" + err);
     }
-    catch (err) {
-      alert('Algo deu errado com o Login' + err)
-    }
-  }
+  };
 
   const handleChange = (event) => {
-    setForm({...form, [event.target.name]: event.target.value})
-  }
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
 
   const validateInput = () => {
-    return validateEmail(form.email) && validatePassword(form.password)
-  }
+    return validateEmail(form.email) && validatePassword(form.password);
+  };
 
   return (
     <Container>
       <Form>
         <h1>Faça o seu Login 👋</h1>
         <Input
-          name='email'
-          placeholder='Digite o seu e-mail'
+          name="email"
+          placeholder="Digite o seu e-mail"
           onChange={handleChange}
-          type='email'
+          type="email"
         />
         <Input
-          name='password'
-          placeholder='Digite a sua senha'
+          name="password"
+          placeholder="Digite a sua senha"
           onChange={handleChange}
-          type='password'
+          type="password"
         />
         <Button
-          type='submit'
-          text='Entrar!'
+          type="submit"
+          text="Entrar!"
           onClick={handleSubmit}
           disabled={loading === true || !validateInput()}
         />
@@ -66,8 +65,7 @@ const Login = () => {
         </SubContainerSign>
       </Form>
     </Container>
-    
-  )
-}
+  );
+};
 
 export default Login;
