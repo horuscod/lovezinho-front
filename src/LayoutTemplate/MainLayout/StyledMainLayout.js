bc1ba77da453e8f245919e6bf94dc638bb7ceb91
horuscod <jessicabueno.contato2022@gmail.com>
2023-11-29 18:41:23 -0300

    adjust background template main

--- src/LayoutTemplate/MainLayout/StyledMainLayout.js
+++ src/LayoutTemplate/MainLayout/StyledMainLayout.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 export const BackgroundBody = styled.div`
   background: #f6f6f6;
+  min-height: 100vh;
 `;
 
 export const ContainerContent = styled.div`
   max-width: 1280px;
   margin: 0 auto;
-    
 `;
 
 export const DynamicContent = styled.div``;
